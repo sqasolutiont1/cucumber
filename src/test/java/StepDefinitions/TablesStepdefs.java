a661bc57af9bcca04524d125fffca53cc7932fe3
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-02-17 22:57:39 -0600

    init 4

--- src/test/java/StepDefinitions/TablesStepdefs.java
+++ src/test/java/StepDefinitions/TablesStepdefs.java
@@ -1,6 +1,8 @@
 package StepDefinitions;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import pageObjects.authenticgoods.Tables;
 
 public class TablesStepdefs {
@@ -8,5 +10,14 @@ public class TablesStepdefs {
     @Given("Navigate to tables")
     public void navigateToTables() {
         tables.navToTables();
+    }
+
+    @When("I check if I'm on the tables page")
+    public void iCheckIfIMOnTheTablesPage() {
+        tables.checkIfOnTheCorrectPage();
+    }
+    @After
+    public void close(){
+        tables.quit();
     }
 }
--- src/test/java/pageObjects/authenticgoods/Tables.java
+++ src/test/java/pageObjects/authenticgoods/Tables.java
@@ -1,12 +1,22 @@
 package pageObjects.authenticgoods;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
 public class Tables extends CommonPage{
     String url = "http://authenticgoods.co/wrapbootstrap/themes/neuboard-v1.4.3/Angular_full_version/#/tables/basic";
+    String header = "Basic Tables";
+
     public Tables(){
         super();
     }
 
     public void navToTables(){
         navigate(url);
+    }
+
+    public void checkIfOnTheCorrectPage() {
+        By locator = By.cssSelector("h1");
+        Assert.assertEquals(getElement(locator).getText(), header);
     }
 }
