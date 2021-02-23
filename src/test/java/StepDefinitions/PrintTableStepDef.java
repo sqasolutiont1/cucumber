c5bb48ba4f9c6f5aa442ad8ac93c265f2b5dbc47
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-02-22 21:28:58 -0600

    init 25

---
+++ src/test/java/StepDefinitions/PrintTableStepDef.java
@@ -0,0 +1,20 @@
+package StepDefinitions;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+import java.util.Map;
+
+public class PrintTableStepDef {
+    @When("Print the Table")
+    public void printTheTable(DataTable dataTable) {
+        List<List<String>> rows = dataTable.asLists(String.class);
+        List<Map<String, String>> map_rows = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> map :  map_rows) {
+            System.out.println(map);
+        }
+
+    }
+}
---
+++ src/test/resources/cucumber_tables.feature
@@ -0,0 +1,8 @@
+Feature: Test Automation for Forms
+  Scenario: Table usage
+    When Print the Table
+    | Fields                 | Values              |
+    | First Name             | Tom                 |
+    | Last Name              | Kenny               |
+
+
