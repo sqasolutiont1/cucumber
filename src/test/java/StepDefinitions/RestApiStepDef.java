532fbc7a20374dbcb019ba13de95913f020332be
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-02-24 21:19:27 -0600

    init 28

--- src/test/java/StepDefinitions/RestApiStepDef.java
+++ src/test/java/StepDefinitions/RestApiStepDef.java
@@ -1,23 +1,25 @@
 package StepDefinitions;
-import io.restassured.RestAssured.*;
+
+import io.cucumber.java.en.When;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.matcher.RestAssuredMatchers.*;
+import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import org.hamcrest.Matchers.*;
-import io.restassured.module.jsv.JsonSchemaValidator.*;
-import io.cucumber.java.en.When;
+import org.apache.commons.io.FileUtils;
+import org.testng.Assert;
+
+import java.io.File;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 public class RestApiStepDef {
     protected static RequestSpecification request;
     protected static ResponseSpecification responseSpec;
 
     @When("I call Get Categories I have a response")
-    public void iCallGetCategoriesIHaveAResponse() {
+    public void iCallGetCategoriesIHaveAResponse() throws IOException {
         request = new RequestSpecBuilder().
                 setBaseUri("https://petstore.swagger.io/v2").
                 build();
@@ -25,7 +27,8 @@ public class RestApiStepDef {
                 spec(request).
                 when().
                 get("/pet/findByStatus?status=available");
-        //System.out.println(response.asString());
-        System.out.println(response.then().extract().body().jsonPath().prettify());
+                int responseCode = response.then().assertThat().extract().response().getStatusCode();
+        Assert.assertEquals(responseCode, 201);
+
     }
 }
