abad5098979efc0f4503c28b13ffd0e11116dc3c
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-02-27 16:58:25 -0600

    init 37

--- src/main/java/ApachePOI.java
+++ src/main/java/ApachePOI.java
@@ -1,28 +1,43 @@
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class ApachePOI {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        /**
+         * this is creation of the book object. Like container for all the sheets
+         */
         XSSFWorkbook workbook = new XSSFWorkbook();
+        /**
+         * We are creating the sheet in that book. and we give a name to that sheet.
+         */
         XSSFSheet sheet = workbook.createSheet("Java Books");
-
+        /**
+         * adding data to the sheet
+         */
         Object[][] bookData = {
-                {"Head First Java", "Kathy Serria", 79},
-                {"Effective Java", "Joshua Bloch", 36},
-                {"Clean Code", "Robert martin", 42},
-                {"Thinking in Java", "Bruce Eckel", 35},
+                {"Head First Java", "Kathy Serria","79"},
+                {"Effective Java", "Joshua Bloch", "36"},
+                {"Clean Code", "Robert martin", "42"},
+                {"Thinking in Java", "Bruce Eckel", "35"},
         };
 
         int rowCount = 0;
 
         for (Object[] aBook : bookData) {
             Row row = sheet.createRow(++rowCount);
-
+            //var++ returns its value before incrementing.
+            //result = var +1;
+            //++var returns its value after incrementing.
+            //var + 1 = result
             int columnCount = 0;
 
             for (Object field : aBook) {
@@ -36,9 +51,16 @@ public class ApachePOI {
 
         }
 
-
-        try (FileOutputStream outputStream = new FileOutputStream("JavaBooks.xlsx")) {
-            workbook.write(outputStream);
+        try (FileOutputStream outputStream = new FileOutputStream("src/main/resources/JavaBooks.xlsx")) {
+            try {
+                workbook.write(outputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
