5d322b8dffdd986ecb9879c3fe1ba857a1b0c4a2
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-02-27 18:15:57 -0600

    init 39

---
+++ src/test/java/pageObjects/authenticgoods/Alerts.java
@@ -0,0 +1,32 @@
+package pageObjects.authenticgoods;
+
+public class Alerts extends CommonPage {
+
+    public Alerts() {
+        super();
+    }
+
+    public void navigateToAlertPage() {
+
+    }
+
+    public void checkHeader() {
+
+    }
+
+    public void fillinTextField() {
+
+    }
+
+    public void pressAddNotificationBtn() {
+
+    }
+
+    public void checkErrorMessage() {
+
+    }
+
+    public void readNotification() {
+
+    }
+}
