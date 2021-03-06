693f10078ac9672a4271338d76262736bebf8645
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-03-06 17:48:38 -0600

    init 76

--- src/test/java/pageObjects/authenticgoods/Mask.java
+++ src/test/java/pageObjects/authenticgoods/Mask.java
@@ -3,7 +3,6 @@ package pageObjects.authenticgoods;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
@@ -13,13 +12,13 @@ import java.io.IOException;
 public class Mask extends CommonPage {
 
     public void setDate(String value) {
-        By locator = By.xpath("//div[@class='panel panel-default']//div[1]//div[1]//input[1]");
+        By locator = By.xpath("/html/body/section/section/div[1]/div/section/div/div/div/div[2]/form/div[1]/div/input");
         getElement(locator).click();
-        getElement(locator).sendKeys(Keys.HOME + value);
+        getElement(locator).sendKeys(value);
     }
 
     public String getDate() {
-        By locator = By.xpath("//div[@class='panel panel-default']//div[1]//div[1]//input[1]");
+        By locator = By.xpath("/html/body/section/section/div[1]/div/section/div/div/div/div[2]/form/div[1]/div/input");
         return getElement(locator).getAttribute("value");
     }
 }
