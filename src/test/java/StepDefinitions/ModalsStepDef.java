a38c7e06a3b76aea88979b165f4ced847563c862
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-03-06 18:41:27 -0600

    init 79

--- src/test/java/StepDefinitions/ModalsStepDef.java
+++ src/test/java/StepDefinitions/ModalsStepDef.java
@@ -3,7 +3,6 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.bouncycastle.math.raw.Mod;
 import org.testng.Assert;
 import pageObjects.authenticgoods.Modals;
 
