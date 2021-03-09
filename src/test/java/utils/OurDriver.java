c4233681906ad93dd83eebb83ffa57fc00128ae9
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-03-08 21:53:46 -0600

    remove massages from start dialog

--- src/test/java/utils/OurDriver.java
+++ src/test/java/utils/OurDriver.java
@@ -3,17 +3,25 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.logging.Level;
 
 public class OurDriver {
     public WebDriver localDriver() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
+        options.addArguments("--silent");
+        options.addArguments("headless");
+        options.addArguments("log-level=3");
+        options.addArguments("--disable-logging");
+        java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
         return new ChromeDriver(options);
     }
 
